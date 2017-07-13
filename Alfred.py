b3aec43450349158005dbd26b062d2a6c6949e16
mateusmforte <m-forte@hotmail.com>
2017-07-13 17:55:10 -0300

    Feito alguns testes com envio de mensagem do bot para o usuário

--- Alfred.py
+++ Alfred.py
@@ -1,1 +1,12 @@
 import telepot
+from pprint import pprint
+from telepot.loop import MessageLoop
+bot = telepot.Bot('354305351:AAEWwnWaZo_Dnn1hOr_PuVCQtuH_WwzAy5A')
+bot.getMe()
+
+response = bot.getUpdates()
+pprint(response)
+def handle(msg):
+    pprint(msg)
+MessageLoop(bot,handle).run_as_thread()
+bot.sendMessage(131404726,'Olá!')
