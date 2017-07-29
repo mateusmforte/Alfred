8d545bb5acebee41bd214b37a75a4e39a652975a
Mateus Moura Forte <m-forte@hotmail.com>
2017-07-29 18:37:37 -0300

    Alfred agora responde com troncadilhos quando chamado Coloquei o método str.upper() na função checkTxtMsg para converter qualquer call para maiusculo

--- Alfred.py
+++ Alfred.py
@@ -2,18 +2,36 @@ import sys
 import time
 import telepot
 from telepot.loop import MessageLoop
+from random import randint
+def trocadilho ():
+    trocadilhos = {
+        1:"Eu sempre me preocupo com aquilo, ainda mais quando se trata de comida a quilo.",
+        2:"Gata, você é Ágata? Sim por quê? Porque você é a gata.",
+        3:"Meu bolso não é aquário, mas está cheio de peixe.",
+        4:"Gata, você não é bebida alcoólica, mas Ice eu te pego.",
+        5:"Encontrei com uma mina, pisei nela e explodiu.",
+        6:"Não dê risada de tudo, porque quem acha tudo gozado é faxineira de motel.",
+        7:"Seu nome é Tais? Não, por quê? Porque taistrovando!",
+        8:"A enxada é de João, a foice é de Mané e o machado de Assis.",
+        9:"Eu sou aquele cara que as minas tudo disputa, diz: puta que pariu que cara feio.",
+        10:"É sempre bom contratar um demônio pra ser seu jardineiro, além de cortar, ele pentagrama."
+    }
+    num = randint(1,10)
+    return(trocadilhos[num])
 def checkTxtMsg(msg):
-    calls = ['Alfred','alfred','Oi Alfred','oi alfred','Hey Alfred','hey alfred']
+    calls = ['ALFRED','OI ALFRED','HEY ALFRED']
     for each in calls:
-        if msg == each:
+        if str.upper(msg) == each:
             return True
+
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
+
     if content_type == 'text' and checkTxtMsg(msg['text']):
-        bot.sendMessage(chat_id,'Ola Amo')
+        bot.sendMessage(chat_id,trocadilho())
 
 bot = telepot.Bot("354305351:AAFAAnzBnsE43UPScoGRXO-a_qjZpEIgZkM")
 MessageLoop(bot, handle).run_as_thread()
