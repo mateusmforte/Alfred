cba50821acb4555970f26de3e293bf9a9c457f18
Mateus Moura Forte <m-forte@hotmail.com>
2017-07-29 17:30:34 -0300

    Alfred agora responde a vários tipos de chamado

--- Alfred.py
+++ Alfred.py
@@ -2,12 +2,17 @@ import sys
 import time
 import telepot
 from telepot.loop import MessageLoop
-calls = ['Alfred','alfred','Oi Alfred','oi alfred','Hey Alfred','hey alfred']
+def checkTxtMsg(msg):
+    calls = ['Alfred','alfred','Oi Alfred','oi alfred','Hey Alfred','hey alfred']
+    for each in calls:
+        if msg == each:
+            return True
+
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
-    if content_type == 'text' and msg['text']  == 'Alfred':
+    if content_type == 'text' and checkTxtMsg(msg['text']):
         bot.sendMessage(chat_id,'Ola Amo')
 
 bot = telepot.Bot("354305351:AAFAAnzBnsE43UPScoGRXO-a_qjZpEIgZkM")
