bdc647c57571cd6b2ef9297a41b4486841289391
Mateus Moura Forte <m-forte@hotmail.com>
2017-07-29 16:26:19 -0300

    Revert "Agora Alfred responde ao usuário quando ele é chamado pelo nome"
    This reverts commit 43afbefee781a3c4e906185bc2d6b45908150915.

--- Alfred.py
+++ Alfred.py
@@ -1,19 +1,12 @@
-import sys
-import time
 import telepot
+from pprint import pprint
 from telepot.loop import MessageLoop
-calls = ['Alfred','alfred','Oi Alfred','oi alfred','Hey Alfred','hey alfred']
+bot = telepot.Bot()
+bot.getMe()
+
+response = bot.getUpdates()
+pprint(response)
 def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
-
-    if content_type == 'text' and msg['text']  == 'Alfred':
-        bot.sendMessage(chat_id,'Ola Amo')
-
-bot = telepot.Bot("354305351:AAFAAnzBnsE43UPScoGRXO-a_qjZpEIgZkM")
-MessageLoop(bot, handle).run_as_thread()
-print ('Listening ...')
-
-# Keep the program running.
-while 1:
-    time.sleep(10)
+    pprint(msg)
+MessageLoop(bot,handle).run_as_thread()
+bot.sendMessage(131404726,'Olá!')
