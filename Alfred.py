4b857490de061f1b26c47ef18f13c6378c8c68e5
Mateus Moura Forte <m-forte@hotmail.com>
2017-09-09 23:54:37 -0300

    Corrigido o bug com os trocadilhos
    Corrigi o erro com o texto dos trocadilhos e adicionei um retorno
    diferente para a função checkCall

--- Alfred.py
+++ Alfred.py
@@ -6,12 +6,11 @@ from random import randint
 def trocadilho ():
     trocadilhos = []
     num = randint(0,99)
-    arquivo = open("C:/Users/m-for/Documents/GitHub/Alfred/Alfred/trocadilhos.txt","r",encoding='utf-8')
-    texto = arquivo.read()
-    trocadilho = texto.split('\n')
-    for linha in trocadilho:
-        l = linha.split(',')
-        trocadilhos.append(l) 
+    arquivo = open("trocadilhos.txt","r",encoding='UTF-8')
+    texto = str(arquivo.read())
+    linha_trocadilho = texto.split('\n')
+    for linha in linha_trocadilho:
+        trocadilhos.append(linha)
     return trocadilhos[num]
 def callAlfred(msg):
     calls = ['ALFRED','OI ALFRED','HEY ALFRED']
@@ -20,13 +19,13 @@ def callAlfred(msg):
             return True
 def checkCall(msg):
     if str.upper(msg) == "ALFRED CONTE-ME UM TROCADILHO":
-        return True
+        return "trocadilho"
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
     if content_type == 'text' and callAlfred(msg['text']):
         bot.sendMessage(chat_id,'Olá Amo')
-    if content_type == 'text' and checkCall(msg['text']):
+    if content_type == 'text' and checkCall(msg['text']) == "trocadilho":
         bot.sendMessage(chat_id,trocadilho())
 bot = telepot.Bot("354305351:AAFAAnzBnsE43UPScoGRXO-a_qjZpEIgZkM")
 MessageLoop(bot, handle).run_as_thread()
